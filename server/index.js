84c4b0f844342e0e3b8ac5809c0935684120bc15
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-16 23:42:14 +0200

    set up build and gracefully shutdown

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const cors = require('cors')
-const { MongoClient, ObjectId } = require('mongodb')
+const { MongoClient } = require('mongodb')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
 
 app.get('/api/searchByWord/:word', (req, res) => {
     req.app.locals.MongoClient.db('cool_data').collection('dictionary').aggregate([
@@ -19,6 +18,22 @@ app.get('/api/searchByWord/:word', (req, res) => {
         res.json(result)
     })
 })
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public_html/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'public_html')))
+
+function gracefulShutdown() {
+    app.locals.MongoClient.close(() => {
+        process.exit()
+    })
+}
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGKILL', gracefulShutdown);
 
 app.listen(8080, () => {
     MongoClient.connect('mongodb://127.0.0.1:27017/?compressors=zlib&directConnection=true&ssl=false').then((client) => {
